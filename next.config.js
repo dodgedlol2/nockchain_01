5c47975cd32f4e50ba316536ce68e87bc2f2f596
dodgedlol2 <dodgedlol2@gmail.com>
2025-07-28 01:52:25 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    // Handle plotly.js imports
+  webpack: (config, { isServer }) => {
+    // Fix for Plotly.js
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        path: false,
+        os: false,
+      }
+    }
+    
+    // Plotly.js alias
     config.resolve.alias = {
       ...config.resolve.alias,
-      'plotly.js': 'plotly.js/dist/plotly.min.js',
+      'plotly.js-dist-min': 'plotly.js-dist-min'
     }
+    
     return config
   }
 }
